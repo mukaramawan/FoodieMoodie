1c88d29b03c2b4e0359fb0eadb2fda16bfa9bbc4
Mukaram Awan <mukaramawan.official@gmail.com>
2024-09-30 14:40:23 +0500

    FlatList & Expo Router

---
+++ src/app/productDetails.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const productDetails = () => {
+  return (
+    <View>
+      <Text>productDetails</Text>
+    </View>
+  )
+}
+
+export default productDetails
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,32 +1,51 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import products from '@/assets/data/products';
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+//import products from '@/assets/data/products';
+import { Product } from "../types";
+import { Link } from "expo-router";
 
-const ProductListItem = ({product}) => {
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png";
+
+type ProductListItemProps = { product: Product };
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-    <Image source={{uri: product.image}} style={styles.image}/>
-    <Text style={styles.title}>{product.name}</Text>
-    <Text style={styles.title}>${product.price}</Text>
-    
-  </View>
+    <Link href={"/productDetails"} asChild>
+
+      <Pressable style={styles.container}>
+
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.title}>${product.price}</Text>
+
+      </Pressable>
+
+    </Link>
   );
-}
+};
 
 export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    margin: 5,
+    maxWidth: "50%",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   image: {
-    width: '100%',
+    width: "100%",
     aspectRatio: 1,
-  }
-
+  },
 });
